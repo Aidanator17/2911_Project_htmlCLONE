8691c83b7519bd64bc21d44dd674a85a14b63dca
Aidan Christopher <gandamovies@gmail.com>
2021-05-21 00:29:14 -0700

    .

--- __tests__/first.test.js
+++ __tests__/first.test.js
@@ -7,7 +7,7 @@ const app = require("../app")
 describe('api', () => {
   describe('get /', () => {
     it('should return a 200', () => {
-      request(app).get('https://chessresults.herokuapp.com/').then((res) => {
+      request("https://chessresults.herokuapp.com").get('/').then((res) => {
         expect(res.statusCode).toBe(200);
       });
     });
@@ -18,7 +18,7 @@ describe('api', () => {
 describe('api', () => {
   describe('get /', () => {
     it('should return a 200', () => {
-      request(app).get('https://chessresults.herokuapp.com/match/e3adf9d5-bc29-43c4-84d7-43b94c17999a').then((res) => {
+      request("https://chessresults.herokuapp.com").get("/match/e3adf9d5-bc29-43c4-84d7-43b94c17999a").then((res) => {
         expect(res.statusCode).toBe(200);
       });
     });
@@ -29,7 +29,7 @@ describe('api', () => {
 describe('api', () => {
   describe('get /', () => {
     it('should return a 404', () => {
-      request(app).get('https://chessresults.herokuapp.com/match/e3adf9d5-bc29-43c4-84d7-43b94c1799b1').then((res) => {
+      request("https://chessresults.herokuapp.com").get('/match/e3adf9d5-bc29-43c4-84d7-43b94c1799b1').then((res) => {
         expect(res.statusCode).toBe(404);
       });
     });
