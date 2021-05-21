f2fe49aafbf1181a0d053523d4782f2468cf71c8
Aidan Christopher <gandamovies@gmail.com>
2021-05-21 01:04:06 -0700

    .

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app")
+const port = process.env.PORT || 8000;
 
-app.set('port', 8000);
-app.listen(app.get('port'), () => {
+app.listen(port, () => {
   console.log(`App is running on http://localhost:${app.get('port')}`)
 });
 
