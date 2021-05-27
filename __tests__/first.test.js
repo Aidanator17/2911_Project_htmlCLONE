defefd7164691559df603ce8199193943178404f
Aidan Christopher <gandamovies@gmail.com>
2021-05-27 11:44:39 -0700

    pretty website

--- __tests__/first.test.js
+++ __tests__/first.test.js
@@ -28,9 +28,9 @@ describe('api', () => {
 
 describe('api', () => {
   describe('get /', () => {
-    it('should return a 404', () => {
+    it('should return a 500', () => {
       request("https://chessresults.herokuapp.com").get('/match/e3adf9d5-bc29-43c4-84d7-43b94c1799b1').then((res) => {
-        expect(res.statusCode).toBe(404);
+        expect(res.statusCode).toBe(500);
       });
     });
   });
